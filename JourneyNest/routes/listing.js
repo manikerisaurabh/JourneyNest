43ae835c804561077e174135daa39c007737b464
manikerisaurabh <manikerisaurabh@gmail.com>
2024-02-17 09:13:03 +0530

    added authorization

--- JourneyNest/routes/listing.js
+++ JourneyNest/routes/listing.js
@@ -4,69 +4,84 @@ const app = express();
 
 const Listing = require("../models/listing");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpessError.js");
-const { listingSchema } = require("../schema.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
-
-//validating the data coming from cliet with the help of joi package
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg)
-    } else {
-        next();
-    }
-}
+const listingController = require("../controllers/listing.js");
 
 //index route
-router.get("/", wrapAsync(async (req, res, next) => {
-    const allListing = await Listing.find()
-    res.render("listings/index.ejs", { allListing });
-    //res.send({ allListing }
-}));
+router.get("/", wrapAsync(listingController.index));
 
 //new listing
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new.ejs")
 });
 
 //show route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const list = await Listing.findById(id).populate("reviews");
-
+    const list = await Listing.findById(id)
+        .populate({
+            path: "reviews",
+            populate: {
+                path: "author"
+            },
+        })
+        .populate("owner");
+    if (!list) {
+        req.flash("error", "Listing you requested does not exits!");
+        res.redirect("/listings");
+    }
+    console.log(list);
     res.render("listings/show.ejs", { list })
 }));
 
 //add new listing into db 
-router.post("/", validateListing, wrapAsync(async (req, res, next) => {
-
+router.post("/", isLoggedIn, validateListing, wrapAsync(async (req, res, next) => {
     let newListing = new Listing(req.body.listing);
+    console.log(req.user);
+    newListing.owner = req.user._id;
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 }));
 
 //edit route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
+router.get("/:id/edit",
+    isLoggedIn,
+    isOwner,
+    wrapAsync(async (req, res) => {
+        let { id } = req.params;
+        const listing = await Listing.findById(id);
+        if (!listing) {
+            req.flash("error", "Listing you requested does not exits!");
+            res.redirect("/listings");
+        }
+        res.render("listings/edit.ejs", { listing });
+    }));
 
 //update edited info into database
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
-    let { id } = req.params
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-}));
+router.put("/:id",
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(async (req, res) => {
+        let { id } = req.params
+
+        await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        req.flash("success", "Listing Updated!");
+        res.redirect(`/listings/${id}`);
+    }));
 
 //delete route
-router.delete("/:id", wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings")
-}));
+router.delete("/:id",
+    isLoggedIn,
+    isOwner,
+    wrapAsync(async (req, res) => {
+        const { id } = req.params;
+        await Listing.findByIdAndDelete(id);
+        req.flash("success", "Listing Deleted!");
+        res.redirect("/listings")
+    }));
 
 
 module.exports = router;
--- JourneyNest/routes/review.js
+++ JourneyNest/routes/review.js
@@ -4,41 +4,41 @@ const router = express.Router({ mergeParams: true });
 
 const Listing = require("../models/listing");
 const wrapAsync = require("../utils/wrapAsync");
-const ExpressError = require("../utils/ExpessError");
 const Review = require("../models/review");
-const { reviewSchema } = require("../schema");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 
-//validating the review coming for the post
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-}
+
 
 //post
-router.post("/", validateReview, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    let listing = await Listing.findById(id);
-    let newReview = new Review(req.body.review)
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    res.redirect(`/listings/${id}`);
-}));
+router.post("/",
+    validateReview,
+    isLoggedIn,
+    wrapAsync(async (req, res) => {
+        const { id } = req.params;
+        let listing = await Listing.findById(id);
+        let newReview = new Review(req.body.review)
+        newReview.author = req.user._id;
+        listing.reviews.push(newReview);
+        console.log(newReview)
+        await newReview.save();
+        await listing.save();
+        req.flash("success", "New Review Created!");
+        res.redirect(`/listings/${id}`);
+    }));
 
 //delete review
-router.delete("/:reviewID", async (req, res) => {
-    console.log("delete review page")
-    const { id, reviewID } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewID } });
-    await Review.findByIdAndDelete(reviewID);
-    res.redirect(`/listings/${id}`);
-})
+router.delete(
+    "/:reviewID",
+    isLoggedIn,
+    isReviewAuthor,
+    async (req, res) => {
+        console.log("delete review page")
+        const { id, reviewID } = req.params;
+        await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewID } });
+        await Review.findByIdAndDelete(reviewID);
+        req.flash("success", "Review Deleted!");
+        res.redirect(`/listings/${id}`);
+    })
 
 module.exports = router;
